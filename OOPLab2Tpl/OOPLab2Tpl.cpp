ba600dba6aef47a549e010e798e901b0a3225504
Arkhiliuk <arkhiliuk.elvira@chnu.edu.ua>
2022-10-16 19:24:59 +0300

    ex1

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -1,38 +1,14 @@
-﻿// OOPLab2Tpl.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <bitset>
 
-#include <iostream>
 using namespace std;
-#include "Tasks.h"
-#include "Examples.h"
 
-
-/// @brief 
-/// @return 
-int main()
-{
-    cout << "OOP. Template for laboratory work #2.\n";
-
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = cin.get();
-        
-        cin.get();
-
-        switch (ch) {
-        case '1': task1();   break;
-        case '2': task1();   break;
-        case '3': task1();   break;
-        case '4': task1();   break;
-		case '5':  Examples(); break;
-		case '6': return 0;
-	    }
-        cout << " Press any key and enter\n";
-        ch = cin.get();
-    } while (ch != '6');
-
+int main(){
+    int a, b, c, d;
+    std::cin>> a >> b >> c >> d;
+    double defaultNum = (12 * a + ((d*15+12*b)/1024) - 12 * c + d * 15);
+    double customNum = ((a << 3) + (a << 2)) + ((((d << 4) - d) + ((d << 3) + (d << 2))) >> 10) - ((c << 3) + (c << 2)) + ((d << 4) - d);
+    std::cout<< defaultNum << endl << customNum <<endl;
     return 0;
 }
-//ffffff
